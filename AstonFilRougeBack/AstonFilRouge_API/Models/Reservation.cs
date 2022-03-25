37e6c5ef13cf0fd34597b54a61fdb41bc01b8ee0
Loic Coornaert <71696114+Loiccoornaert27@users.noreply.github.com>
2022-03-25 11:41:48 +0100

    Create Reservation.cs

---
+++ AstonFilRougeBack/AstonFilRouge_API/Models/Reservation.cs
@@ -0,0 +1,13 @@
+﻿using static AstonFilRouge_API.Enums.Enumerables;
+
+namespace AstonFilRouge_API.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public Course Course { get; set; }
+        public ReservationStatus Status { get; set; }
+        public DateTime RequestDate { get; set; }
+    }
+}
