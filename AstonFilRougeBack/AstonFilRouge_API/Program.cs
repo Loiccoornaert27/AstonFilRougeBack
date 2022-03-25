70dd4d73046842cf1d838b55eb549390f778cf82
Loic Coornaert <71696114+Loiccoornaert27@users.noreply.github.com>
2022-03-25 14:53:24 +0100

    Update Program.cs

--- AstonFilRougeBack/AstonFilRouge_API/Program.cs
+++ AstonFilRougeBack/AstonFilRouge_API/Program.cs
@@ -1,3 +1,10 @@
+using AstonFilRouge_API.Models;
+using AstonFilRouge_API.Datas;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,37 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
+    .LogTo(Console.WriteLine, LogLevel.Information));
+builder.Services.AddScoped<IRepository<User>, UserListRepository>();
+
+builder.Services.AddCors(options => {
+    options.AddPolicy("allConnections", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["JWT:SecretKey"])),
+        ValidateLifetime = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JWT:ValidAudience"],
+        ValidateIssuer = true,
+        ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+        ClockSkew = TimeSpan.Zero
+
+    };
+});
 
 var app = builder.Build();
 
