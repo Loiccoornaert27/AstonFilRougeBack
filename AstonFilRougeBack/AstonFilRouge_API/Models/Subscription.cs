45ff2651931e21bccbaf44029c045de0c3dc220b
Loic Coornaert <71696114+Loiccoornaert27@users.noreply.github.com>
2022-03-25 11:53:07 +0100

    Create Subscription.cs

---
+++ AstonFilRougeBack/AstonFilRouge_API/Models/Subscription.cs
@@ -0,0 +1,16 @@
+﻿using static AstonFilRouge_API.Enums.Enumerables;
+
+namespace AstonFilRouge_API.Models
+{
+    public class Subscription
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public int ClubId { get; set; }
+        public int Price { get; set; }
+        public BillingPeriod BillingPeriod { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndCommitmentDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
