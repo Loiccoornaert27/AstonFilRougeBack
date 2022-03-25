b5df6da28c9c218eced8964a7f2a8ec7a2514cfb
Loic Coornaert <71696114+Loiccoornaert27@users.noreply.github.com>
2022-03-25 14:24:01 +0100

    Create ClubListRepository.cs

---
+++ AstonFilRougeBack/AstonFilRouge_API/Datas/ClubListRepository.cs
@@ -0,0 +1,50 @@
+﻿using AstonFilRouge_API.Models;
+
+namespace AstonFilRouge_API.Datas
+{
+    public class ClubListRepository : BaseRepository, IRepository<Club>
+    {
+        public ClubListRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public Club Add(Club entity)
+        {
+            _context.ClubList.Add(entity);
+
+            if (_context.SaveChanges() > 0) return GetById(entity.Id);
+            return null;
+        }
+
+        public bool Delete(int id)
+        {
+            _context.ClubList.Remove(GetById(id));
+            if (_context.SaveChanges() > 0) return true;
+            return false;
+        }
+
+        public IEnumerable<Club> GetAll()
+        {
+            return _context.ClubList;
+        }
+
+        public Club GetById(int id)
+        {
+            return _context.ClubList.FirstOrDefault(x => x.Id == id);
+        }
+
+        public Club Update(Club entity)
+        {
+            Club found = GetById(entity.Id);
+            if (found != null)
+            {
+                found.Name = entity.Name;
+
+                _context.ClubList.Update(found);
+            }
+
+            if (_context.SaveChanges() > 0) return GetById(entity.Id);
+            return null;
+        }
+    }
+}
