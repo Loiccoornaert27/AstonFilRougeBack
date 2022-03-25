7fa58b9e86d157712236887a4a1d71e09e2fce22
Loic Coornaert <71696114+Loiccoornaert27@users.noreply.github.com>
2022-03-25 11:09:44 +0100

    Create Enumerables.cs

---
+++ AstonFilRougeBack/AstonFilRouge_API/Enums/Enumerables.cs
@@ -0,0 +1,54 @@
+﻿namespace AstonFilRouge_API.Enums
+{
+    public class Enumerables
+    {
+        public enum BillingPeriod
+        {
+            Monthly,
+            Annually
+        }
+        public enum CourseStatus
+        {
+            Pending,
+            Planned,
+            Cancelled,
+            InProgress,
+            Over
+        }
+        public enum CourseType
+        {
+            Access,
+            Individual,
+            Collective,
+            Home
+        }
+
+        public enum Day
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        public enum ReservationStatus
+        {
+            Requested,
+            Rejected,
+            Validated
+        }
+
+        public enum Role
+        {
+            Guest,
+            Member,
+            Coach,
+            Manager,
+            SuperAdmin,
+            All
+        }
+    }
+}
