1fa822c1908be691e108c92a41bcb2557827fa2a
Loic Coornaert <71696114+Loiccoornaert27@users.noreply.github.com>
2022-03-25 11:32:54 +0100

    Update Course.cs

--- AstonFilRougeBack/AstonFilRouge_API/Models/Course.cs
+++ AstonFilRougeBack/AstonFilRouge_API/Models/Course.cs
@@ -1,6 +1,19 @@
-﻿namespace AstonFilRouge_API.Models
+﻿using static AstonFilRouge_API.Enums.Enumerables;
+
+namespace AstonFilRouge_API.Models
 {
     public class Course
     {
+        public int Id { get; set; }
+        public CourseType Type { get; set; }
+        public DateTime Date { get; set; }
+        public int StartHour { get => Date.Hour; }
+        public int EndHour { get; set; }  
+        public CourseStatus Status { get; set; }
+        public int ClubId { get; set; }
+        public Address Address { get; set; }
+        public Coach CoachId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
