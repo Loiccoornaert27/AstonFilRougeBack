c8677b08cc4708d66d364c7487c66cb3f344dada
Loic Coornaert <71696114+Loiccoornaert27@users.noreply.github.com>
2022-03-25 10:32:34 +0100

    Create Address.cs

---
+++ AstonFilRougeBack/AstonFilRouge_API/Models/Address.cs
@@ -0,0 +1,13 @@
+﻿namespace AstonFilRouge_API.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public int Num { get; set; }
+        public string Street { get; set; }
+        public string Complement { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+
+    }
+}
