63d3c9592564ef3d4c76c69611a14be76b49eb06
Loic Coornaert <71696114+Loiccoornaert27@users.noreply.github.com>
2022-03-25 14:38:20 +0100

    Create OpeningDayListRepository.cs

---
+++ AstonFilRougeBack/AstonFilRouge_API/Datas/OpeningDayListRepository.cs
@@ -0,0 +1,50 @@
+﻿using AstonFilRouge_API.Models;
+
+namespace AstonFilRouge_API.Datas
+{
+    public class OpeningDayListRepository : BaseRepository, IRepository<OpeningDay>
+    {
+        public OpeningDayListRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public OpeningDay Add(OpeningDay entity)
+        {
+            _context.OpeningDayList.Add(entity);
+
+            if (_context.SaveChanges() > 0) return GetById(entity.Id);
+            return null;
+        }
+
+        public bool Delete(int id)
+        {
+            _context.OpeningDayList.Remove(GetById(id));
+            if (_context.SaveChanges() > 0) return true;
+            return false;
+        }
+
+        public IEnumerable<OpeningDay> GetAll()
+        {
+            return _context.OpeningDayList;
+        }
+
+        public OpeningDay GetById(int id)
+        {
+            return _context.OpeningDayList.FirstOrDefault(x => x.Id == id);
+        }
+
+        public OpeningDay Update(OpeningDay entity)
+        {
+            OpeningDay found = GetById(entity.Id);
+            if (found != null)
+            {
+                found.Day = entity.Day;
+
+                _context.OpeningDayList.Update(found);
+            }
+
+            if (_context.SaveChanges() > 0) return GetById(entity.Id);
+            return null;
+        }
+    }
+}
