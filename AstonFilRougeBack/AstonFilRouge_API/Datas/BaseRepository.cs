0a74793803c86d1b4ddb1764cae296201bd1dec2
Loic Coornaert <71696114+Loiccoornaert27@users.noreply.github.com>
2022-03-25 12:27:55 +0100

    implémentation baserepo et irepo

---
+++ AstonFilRougeBack/AstonFilRouge_API/Datas/BaseRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace AstonFilRouge_API.Datas
+{
+    public abstract class BaseRepository
+    {
+        protected ApplicationDbContext _context;
+
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ AstonFilRougeBack/AstonFilRouge_API/Datas/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace AstonFilRouge_API.Datas
+{
+    public interface IRepository<T> where T : class
+    {
+        public T Add(T entity);
+        public T GetById(int id);
+        public IEnumerable<T> GetAll();
+        public T Update(T entity);
+        public bool Delete(int id);
+    }
+}
