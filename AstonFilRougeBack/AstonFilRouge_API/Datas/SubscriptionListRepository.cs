c86c8df0f229201b211c1b398e21515e6ea95501
Loic Coornaert <71696114+Loiccoornaert27@users.noreply.github.com>
2022-03-25 14:43:43 +0100

    Create SubscriptionListRepository.cs

---
+++ AstonFilRougeBack/AstonFilRouge_API/Datas/SubscriptionListRepository.cs
@@ -0,0 +1,50 @@
+﻿using AstonFilRouge_API.Models;
+
+namespace AstonFilRouge_API.Datas
+{
+    public class SubscriptionListRepository : BaseRepository, IRepository<Subscription>
+    {
+        public SubscriptionListRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public Subscription Add(Subscription entity)
+        {
+            _context.SubscriptionList.Add(entity);
+
+            if (_context.SaveChanges() > 0) return GetById(entity.Id);
+            return null;
+        }
+
+        public bool Delete(int id)
+        {
+            _context.SubscriptionList.Remove(GetById(id));
+            if (_context.SaveChanges() > 0) return true;
+            return false;
+        }
+
+        public IEnumerable<Subscription> GetAll()
+        {
+            return _context.SubscriptionList;
+        }
+
+        public Subscription GetById(int id)
+        {
+            return _context.SubscriptionList.FirstOrDefault(x => x.Id == id);
+        }
+
+        public Subscription Update(Subscription entity)
+        {
+            Subscription found = GetById(entity.Id);
+            if (found != null)
+            {
+                found.ClientId = entity.ClientId;
+
+                _context.SubscriptionList.Update(found);
+            }
+
+            if (_context.SaveChanges() > 0) return GetById(entity.Id);
+            return null;
+        }
+    }
+}
