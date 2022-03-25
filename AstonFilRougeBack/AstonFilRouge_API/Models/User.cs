42107e46addadeb054b50a25bf9cb72f1228b462
Loic Coornaert <71696114+Loiccoornaert27@users.noreply.github.com>
2022-03-25 12:02:01 +0100

    Create User.cs

---
+++ AstonFilRougeBack/AstonFilRouge_API/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using static AstonFilRouge_API.Enums.Enumerables;
+
+namespace AstonFilRouge_API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Address Address { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Role Role { get; set; }
+
+        public string Job { get; set; }
+        public int ClubId { get; set; }
+        public virtual Club Club { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
