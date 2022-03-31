220658840961db9a4e579f7f31cdda56356f6a65
Loic Coornaert <71696114+Loiccoornaert27@users.noreply.github.com>
2022-03-31 10:07:34 +0200

    Create CourseController.cs

---
+++ AstonFilRougeBack/AstonFilRouge_API/Controllers/CourseController.cs
@@ -0,0 +1,113 @@
+﻿using AstonFilRouge_API.Datas;
+using AstonFilRouge_API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AstonFilRouge_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly IRepository<Course> _courseRepo;
+
+        public CourseController(IRepository<Course> courseRepo)
+        {
+            _courseRepo = courseRepo;
+        }
+
+        [HttpGet("/courseList")]
+        public IActionResult GetAllCourses()
+        {
+            return Ok(new
+            {
+                CourseList = _courseRepo.GetAll()
+            });
+        }
+
+        [HttpGet("/courseList/{id}")]
+        public IActionResult GetCourseById(int id)
+        {
+            Course found = _courseRepo.GetById(id);
+            if (found == null)
+            {
+                return NotFound(new
+                {
+                    Message = "Aucune séance avec cet id trouvée."
+                });
+            }
+            return Ok(new
+            {
+                Message = "Séance trouvée",
+                Course = found
+            });
+        }
+
+        [HttpPost("/courseList")]
+        [Authorize(Roles = "Coach")]
+        public IActionResult CreateNewCourse([FromBody] Course newCourse)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            if (_courseRepo.Add(newCourse) != null)
+            {
+                return Ok(new
+                {
+                    Mesage = "Nouvelle séance ajoutée avec succès."
+                });
+            }
+            else
+            {
+                ModelState.AddModelError("Add Course", "Oops il y a eu un problème lors de l'ajout de la séance");
+                return BadRequest(ModelState);
+            }
+        }
+        [HttpDelete("courseList/{id}")]
+        public IActionResult DeleteCourse(int id)
+        {
+            Course found = _courseRepo.GetById(id);
+            if (found == null) return NotFound(new
+            {
+                Message = "Aucune séance avec cet id trouvée."
+            });
+            if (_courseRepo.Delete(id)) return Ok(new
+            {
+                Message = "Séance supprimée avec succès"
+            });
+            else
+            {
+                return BadRequest(new
+                {
+                    Message = "Erreur lors de la suppression de la séance."
+                });
+            }
+        }
+
+        [HttpPatch("/clubList/{id}")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult EditClub(int id, [FromBody] Course editedCourse)
+        {
+            var found = _courseRepo.GetById(id);
+            if (found == null) return NotFound(new
+            {
+                Message = "Aucune séance avec cet id trouvée."
+            });
+
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            if (_courseRepo.Update(id, editedCourse) != null)
+            {
+                return Ok(new
+                {
+                    Message = "Séance modifiée avec succès.",
+                    Course = _courseRepo.GetById(id)
+                });
+            }
+            else
+            {
+                ModelState.AddModelError("Editing Course", "Oops. Il y a eu un problème lors de la modification de la séance");
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
