ebab648773b2f4802fcebf7973d384fe52a79288
Loic Coornaert <71696114+Loiccoornaert27@users.noreply.github.com>
2022-03-31 11:11:08 +0200

    Create ReservationController.cs

---
+++ AstonFilRougeBack/AstonFilRouge_API/Controllers/ReservationController.cs
@@ -0,0 +1,110 @@
+﻿using AstonFilRouge_API.Datas;
+using AstonFilRouge_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AstonFilRouge_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        private readonly IRepository<Reservation> _resaRepo;
+
+        public ReservationController(IRepository<Reservation> resaRepo)
+        {
+            _resaRepo = resaRepo;
+        }
+
+        [HttpGet("/reservationList")]
+        public IActionResult GetAllReservations()
+        {
+            return Ok(new
+            {
+                ReservationList = _resaRepo.GetAll()
+            });
+        }
+
+        [HttpGet("/reservationList/{id}")]
+        public IActionResult GetReservationById(int id)
+        {
+            Reservation found = _resaRepo.GetById(id);
+            if (found == null)
+            {
+                return NotFound(new
+                {
+                    Message = "Aucune réservation avec cet id trouvée."
+                });
+            }
+            return Ok(new
+            {
+                Message = "Réservation trouvée",
+                Reservation = found
+            });
+        }
+
+        [HttpPost("/reservationList")]
+        public IActionResult CreateNewReservation([FromBody] Reservation newResa)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            if (_resaRepo.Add(newResa) != null)
+            {
+                return Ok(new
+                {
+                    Mesage = "Nouvelle réservation ajoutée avec succès."
+                });
+            }
+            else
+            {
+                ModelState.AddModelError("Add Reservation", "Oops il y a eu un problème lors de l'ajout de la réservation.");
+                return BadRequest(ModelState);
+            }
+        }
+        [HttpDelete("reservationList/{id}")]
+        public IActionResult DeleteReservation(int id)
+        {
+            Reservation found = _resaRepo.GetById(id);
+            if (found == null) return NotFound(new
+            {
+                Message = "Aucune réservation avec cet id trouvée."
+            });
+            if (_resaRepo.Delete(id)) return Ok(new
+            {
+                Message = "Réservation supprimée avec succès"
+            });
+            else
+            {
+                return BadRequest(new
+                {
+                    Message = "Erreur lors de la suppression de la réservation."
+                });
+            }
+        }
+
+        [HttpPatch("/reservationList/{id}")]
+        public IActionResult EditReservation(int id, [FromBody] Reservation editedResa)
+        {
+            var found = _resaRepo.GetById(id);
+            if (found == null) return NotFound(new
+            {
+                Message = "Aucune réservation avec cet id trouvée."
+            });
+
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            if (_resaRepo.Update(id, editedResa) != null)
+            {
+                return Ok(new
+                {
+                    Message = "Reservation modifiée avec succès.",
+                    Reservation = _resaRepo.GetById(id)
+                });
+            }
+            else
+            {
+                ModelState.AddModelError("Editing Reservation", "Oops. Il y a eu un problème lors de la modification de la réservation");
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
