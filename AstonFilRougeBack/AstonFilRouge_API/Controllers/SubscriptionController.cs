902548b59beccabd3dc3c48f621ea4a9bdbdf978
Loic Coornaert <71696114+Loiccoornaert27@users.noreply.github.com>
2022-03-31 11:25:38 +0200

    Create SubscriptionController.cs

---
+++ AstonFilRougeBack/AstonFilRouge_API/Controllers/SubscriptionController.cs
@@ -0,0 +1,111 @@
+﻿using AstonFilRouge_API.Datas;
+using AstonFilRouge_API.Models;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AstonFilRouge_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubscriptionController : ControllerBase
+    {
+        private readonly IRepository<Subscription> _subRepo;
+
+        public SubscriptionController(IRepository<Subscription> subRepo)
+        {
+            _subRepo = subRepo;
+        }
+
+        [HttpGet("/subscriptionList")]
+        public IActionResult GetAllSubscriptions()
+        {
+            return Ok(new
+            {
+                SubscriptionList = _subRepo.GetAll()
+            });
+        }
+
+        [HttpGet("/subscriptionList/{id}")]
+        public IActionResult GetSubscriptionById(int id)
+        {
+            Subscription found = _subRepo.GetById(id);
+            if (found == null)
+            {
+                return NotFound(new
+                {
+                    Message = "Aucun abonnement avec cet id trouvée."
+                });
+            }
+            return Ok(new
+            {
+                Message = "Abonnement trouvé",
+                Address = found
+            });
+        }
+
+        [HttpPost("/subscriptionList")]
+        public IActionResult CreateNewSubscription([FromBody] Subscription newSub)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            if (_subRepo.Add(newSub) != null)
+            {
+                return Ok(new
+                {
+                    Mesage = "Nouvel abonnement ajouté avec succès."
+                });
+            }
+            else
+            {
+                ModelState.AddModelError("Add Subscription", "Oops. Il y a eu un problème lors de l'ajout de l'abonnement");
+                return BadRequest(ModelState);
+            }
+        }
+        [HttpDelete("subscriptionList/{id}")]
+        public IActionResult DeleteSubscription(int id)
+        {
+            Subscription found = _subRepo.GetById(id);
+            if (found == null) return NotFound(new
+            {
+                Message = "Aucun abonnement avec cet id trouvé."
+            });
+            if (_subRepo.Delete(id)) return Ok(new
+            {
+                Message = "Abonnement supprimé avec succès"
+            });
+            else
+            {
+                return BadRequest(new
+                {
+                    Message = "Erreur lors de la suppression de l'abonnement."
+                });
+            }
+        }
+
+        [HttpPatch("/subscriptionList/{id}")]
+        public IActionResult EditSubscription(int id, [FromBody] Subscription editedSub)
+        {
+            var found = _subRepo.GetById(id);
+            if (found == null) return NotFound(new
+            {
+                Message = "Aucun abonnement avec cet id trouvée."
+            });
+
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            if (_subRepo.Update(id, editedSub) != null)
+            {
+                return Ok(new
+                {
+                    Message = "Abonnement modifié avec succès.",
+                    Subscription = _subRepo.GetById(id)
+                });
+            }
+            else
+            {
+                ModelState.AddModelError("Editing Subscription", "Oops. Il y a eu un problème lors de la modification de l'abonnement");
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
