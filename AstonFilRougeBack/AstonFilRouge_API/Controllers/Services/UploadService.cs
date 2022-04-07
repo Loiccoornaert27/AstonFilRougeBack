af08294d7bfe596dc5e074eff50d98e69baa31d1
Loic Coornaert <71696114+Loiccoornaert27@users.noreply.github.com>
2022-04-07 16:10:56 +0200

    Ajout de l'upload service pour ajouter des images

---
+++ AstonFilRougeBack/AstonFilRouge_API/Controllers/Services/UploadService.cs
@@ -0,0 +1,26 @@
+﻿namespace AstonFilRouge_API.Controllers.Services
+{
+    public class UploadService
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public UploadService(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public string UploadPicture(IFormFile picture, string folderName)
+        {
+            Directory.CreateDirectory(Path.Combine(_webHostEnvironment.WebRootPath, folderName));
+
+            string alteredName = Guid.NewGuid().ToString() + Path.GetExtension(picture.FileName);
+
+            string path = Path.Combine(_webHostEnvironment.WebRootPath, folderName, alteredName);
+            Stream stream = File.Create(path);
+            picture.CopyTo(stream);
+            stream.Close();
+
+            return folderName + "/" + alteredName;
+        }
+    }
+}
