25c89ce21503c557ffe25646b22d8d2fd277db46
Loic Coornaert <71696114+Loiccoornaert27@users.noreply.github.com>
2022-04-11 10:05:37 +0200

    Ajout reporting service + calcul du chiffre d'affaire mensuel

---
+++ AstonFilRougeBack/AstonFilRouge_API/Controllers/Services/ReportingService.cs
@@ -0,0 +1,34 @@
+﻿using AstonFilRouge_API.Models;
+using AstonFilRouge_API.Datas;
+
+namespace AstonFilRouge_API.Controllers.Services
+{
+    public class ReportingService
+    {
+        private IRepository<Reservation> _resaRepo;
+        private IRepository<Subscription> _subRepo;
+        private IRepository<Club> _clubRepo;
+
+        public int GetMonthlyRevenues(int id)
+        {
+            Club club = _clubRepo.GetById(id);
+            int revenues = 0;
+            IEnumerable<Subscription> subList = _subRepo.GetAll();
+            foreach(Subscription sub in subList )
+            {
+                if(sub.ClubId == id)
+                {
+                    if (sub.BillingPeriod == 0)
+                    {
+                        revenues += sub.Price;
+                    }
+                    else if(sub.BillingPeriod == 1)
+                    {
+                        revenues += sub.Price / 12;
+                    }
+                }
+            }
+            return revenues;
+        }
+    }
+}
