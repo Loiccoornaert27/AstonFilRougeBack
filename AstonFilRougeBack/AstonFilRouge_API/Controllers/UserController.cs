0d04c99549402759cd177dca30e1f0b6d092dcc0
ThibaultMontois <thibault.montois@gmail.com>
2022-04-12 16:01:47 +0200

    Update UserController.cs

--- AstonFilRougeBack/AstonFilRouge_API/Controllers/UserController.cs
+++ AstonFilRougeBack/AstonFilRouge_API/Controllers/UserController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-using AstonFilRouge_API.Datas;
+﻿using AstonFilRouge_API.Datas;
 using AstonFilRouge_API.Models;
 using Microsoft.AspNetCore.Mvc;
 
